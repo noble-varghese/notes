c052db44c4d3b8b8afe66fa80aee3a5f37891f62
noble-varghese <noblekvarghese96@gmail.com>
2024-10-19 11:10:13 +0530

    fix: Updated the base-url

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "noblevarghese.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
